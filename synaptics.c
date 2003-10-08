4e4f3e83856fed8d0f16fff0bc885152273d7e85
Peter Osterlund <petero2@telia.com>
2003-10-08 19:44:20 +0200

    Changed version to 0.11.8.

--- synaptics.c
+++ synaptics.c
@@ -107,7 +107,7 @@ typedef enum {
 #define DEV_INPUT_EVENT "/dev/input"
 #define EVENT_DEV_NAME "event"
 
-#define VERSION "0.11.7"
+#define VERSION "0.11.8"
 
 /*****************************************************************************
  * Forward declaration
