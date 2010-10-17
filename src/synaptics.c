quadpixels/three-finger-dragging

commit 0b28a4c046a30ccb83278ea0aa59541fad89092c

track abs/rel mode changes in SwitchMode() instead of ignoring them
Signed-off-by: Joe Shaw <joe@joeshaw.org>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/synaptics.c
@@ -2511,8 +2511,25 @@ CloseProc(InputInfoPtr pInfo)
 static int
 SwitchMode(ClientPtr client, DeviceIntPtr dev, int mode)
 {
+    LocalDevicePtr local = (LocalDevicePtr) dev->public.devicePrivate;
+    SynapticsPrivate *priv = (SynapticsPrivate *) (local->private);
+
     DBG(3, "SwitchMode called\n");
-    return (mode == Relative) ? Success : XI_BadMode;
+
+    switch (mode) {
+    case Absolute:
+        priv->absolute_events = TRUE;
+        break;
+
+    case Relative:
+        priv->absolute_events = FALSE;
+        break;
+
+    default:
+        return XI_BadMode;
+    }
+
+    return Success;
 }
 
 static Bool

src/synapticsstr.h
@@ -180,6 +180,7 @@ typedef struct _SynapticsPrivateRec
 
     struct CommData comm;
 
+    Bool absolute_events;               /* post absolute motion events instead of relative */
     SynapticsMoveHistRec move_hist[SYNAPTICS_MOVE_HISTORY]; /* movement history */
     int hist_index;			/* Last added entry in move_hist[] */
     int scroll_y;			/* last y-scroll position */

commit 26d5eaeb6b9321ad1a794846333a60125df7317d

Replace two LocalDevicePtr with InputInfoPtr.
ABI 12 removes the LocalDevicePtr define from the server, InputInfoPtr is
the replacement.
Compile error introduced in 0b28a4c046a30ccb83278ea0aa59541fad89092c.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/synaptics.c
@@ -2520,8 +2520,8 @@ CloseProc(InputInfoPtr pInfo)
 static int
 SwitchMode(ClientPtr client, DeviceIntPtr dev, int mode)
 {
-    LocalDevicePtr local = (LocalDevicePtr) dev->public.devicePrivate;
-    SynapticsPrivate *priv = (SynapticsPrivate *) (local->private);
+    InputInfoPtr pInfo = (InputInfoPtr) dev->public.devicePrivate;
+    SynapticsPrivate *priv = (SynapticsPrivate *) (pInfo->private);
 
     DBG(3, "SwitchMode called\n");
 