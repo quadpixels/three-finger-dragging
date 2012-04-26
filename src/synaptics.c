quadpixels/three-finger-dragging

commit cddab79c408db3b13905a2be72aff4f7bf1406f8

Fix scoll increment for non-smooth scrolling (#46617)
Introduced in 2603ad69b997c999404ecc441e0d64ea2cc22018. Previously, the
delta was incremented by (actual delta/scroll dist), button clicks where
then sent while (delta-- >= 1).

After 2603ad69b997c999404ecc441e0d64ea2cc22018, the delta was incremented by
the delta only, scroll dist was used as increment in the smooth scrolling
information. On non-smooth-scrolling servers, the driver would now send
too many events. Restore the old behaviour by dividing by the scroll
distance before entering the button click loop.

X.Org Bug 46617 <http://bugs.freedesktop.org/show_bug.cgi?id=46617>

Tested-by: Peter Zotov <whitequark@whitequark.org>
Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/synaptics.c
@@ -2512,6 +2512,12 @@ post_scroll_events(const InputInfoPtr pInfo)
     if (valuator_mask_num_valuators(priv->scroll_events_mask))
         xf86PostMotionEventM(pInfo->dev, FALSE, priv->scroll_events_mask);
 #else
+    SynapticsParameters *para = &priv->synpara;
+
+    /* smooth scrolling uses the dist as increment */
+    priv->scroll.delta_y /= para->scroll_dist_vert;
+    priv->scroll.delta_x /= para->scroll_dist_horiz;
+
     while (priv->scroll.delta_y <= -1.0)
     {
         post_button_click(pInfo, 4);

commit 1a76d9f00e1e54ba912a47aa665968e0cfe1b8a0

Don't unconditionally divide by scroll_dist_vert (#46617)
Regression introduced in cddab79c408db3b13905a2be72aff4f7bf1406f8.

If an event has a delta of less than scroll_dist_vert, the delta is
unconditionally divided by the distance, leaving some remainder close to 0
and never actually triggering the scroll amount.

Fix this by working with the increment, not the normalised values.

X.Org Bug 46617 <http://bugs.freedesktop.org/show_bug.cgi?id=46617>

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
Reviewed-by: Chase Douglas <chase.douglas@canonical.com>

src/synaptics.c
@@ -2864,31 +2864,29 @@ post_scroll_events(const InputInfoPtr pInfo)
     SynapticsParameters *para = &priv->synpara;
 
     /* smooth scrolling uses the dist as increment */
-    priv->scroll.delta_y /= para->scroll_dist_vert;
-    priv->scroll.delta_x /= para->scroll_dist_horiz;
 
-    while (priv->scroll.delta_y <= -1.0)
+    while (priv->scroll.delta_y <= -para->scroll_dist_vert)
     {
         post_button_click(pInfo, 4);
-        priv->scroll.delta_y += 1.0;
+        priv->scroll.delta_y += para->scroll_dist_vert;
     }
 
-    while (priv->scroll.delta_y >= 1.0)
+    while (priv->scroll.delta_y >= para->scroll_dist_vert)
     {
         post_button_click(pInfo, 5);
-        priv->scroll.delta_y -= 1.0;
+        priv->scroll.delta_y -= para->scroll_dist_vert;
     }
 
-    while (priv->scroll.delta_x <= -1.0)
+    while (priv->scroll.delta_x <= -para->scroll_dist_horiz)
     {
         post_button_click(pInfo, 6);
-        priv->scroll.delta_x += 1.0;
+        priv->scroll.delta_x += para->scroll_dist_horiz;
     }
 
-    while (priv->scroll.delta_x >= 1.0)
+    while (priv->scroll.delta_x >= para->scroll_dist_horiz)
     {
         post_button_click(pInfo, 7);
-        priv->scroll.delta_x -= 1.0;
+        priv->scroll.delta_x -= para->scroll_dist_horiz;
     }
 #endif
 }
@@ -2934,13 +2932,13 @@ repeat_scrollbuttons(const InputInfoPtr pInfo,
 		id = ffs(change);
 		change &= ~(1 << (id - 1));
 		if (id == 4)
-		    priv->scroll.delta_y -= 1.0;
+		    priv->scroll.delta_y -= para->scroll_dist_vert;
 		else if (id == 5)
-		    priv->scroll.delta_y += 1.0;
+		    priv->scroll.delta_y += para->scroll_dist_vert;
 		else if (id == 6)
-		    priv->scroll.delta_x -= 1.0;
+		    priv->scroll.delta_x -= para->scroll_dist_horiz;
 		else if (id == 7)
-		    priv->scroll.delta_x += 1.0;
+		    priv->scroll.delta_x += para->scroll_dist_horiz;
 	    }
 
 	    priv->nextRepeat = now + repeat_delay;